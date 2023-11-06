dc645dac15817e517e5cabebb4bd02242b71fe23
Abudagir <kondaktor3@gmail.com>
2023-11-06 19:08:59 +0100

    react_home9

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement/*, reset */ } from "./redux/counterSlice";
 import "./App.css";
-import Counter from "./counter/Counter";
 
-// export const Context = createContext(null);
 
 function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector((store) => store.counter)
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+  // const handleReset = () => {
+  //   dispatch(reset());
+  // };
+
   return (
     <div>
-      <Counter/>
+      <h1>Counter value: {counter.value}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      {/* <button onClick={handleReset}>Reset</button> */}
     </div>
   );
 }
